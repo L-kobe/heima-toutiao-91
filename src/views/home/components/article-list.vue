73c561ffbbf2f3ea55b381f4faa7b5b03d63d663
kb <kobe@qq.com>
2020-01-15 18:08:14 +0800

    article-list实现下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="scroll-wrapper">
-    <van-list v-model="upLoading" @load="onLoad" :finished="finished" finished-text="没有更多了">
-      <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
-    </van-list>
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="refreshSuccessText">
+      <van-list v-model="upLoading" @load="onLoad" :finished="finished" finished-text="没有更多了">
+        <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -13,7 +15,9 @@ export default {
     return {
       upLoading: false,
       finished: false,
-      articles: []
+      articles: [],
+      refreshSuccessText: '更新成功', // 文本
+      downLoading: false
     }
   },
   methods: {
@@ -29,6 +33,15 @@ export default {
           this.articles.push(...arr)
           this.upLoading = false
         }
+      }, 1000)
+    },
+    onRefresh () {
+      // 触发下拉刷新
+      setTimeout(() => {
+        let arr = Array.from(Array(10), (value, index) => '追加' + (index + 1))
+        this.articles.unshift(...arr) // 将数据添加到队首
+        this.downLoading = false // 关掉了下拉状态
+        this.refreshSuccessText = `更新了${arr.length}条数据`
       }, 1000)
     }
   }
