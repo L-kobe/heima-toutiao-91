122f84834d140726f8d0718d92f23f081bc7be8c
kb <kobe@qq.com>
2020-02-15 18:42:16 +0800

    个人中心-首页-退出登录

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -40,13 +40,14 @@
       <van-cell icon="edit" title="编辑资料" to="/user/profile" is-link />
       <van-cell icon="chat-o" title="小智同学" to="/user/chat" is-link />
       <van-cell icon="setting-o" title="系统设置" is-link />
-      <van-cell icon="warning-o" title="退出登录" to="/login" is-link />
+      <van-cell icon="warning-o" title="退出登录" @click="lgout" is-link />
     </van-cell-group>
   </div>
 </template>
 
 <script>
 import { getUserInfo } from '@/api/user'
+import { mapMutations } from 'vuex'
 export default {
   name: 'user',
   data () {
@@ -55,8 +56,23 @@ export default {
     }
   },
   methods: {
+    ...mapMutations(['clearUser']),
     async getUserInfo () {
       this.userInfo = await getUserInfo()
+    },
+    // 退出方法
+    async lgout () {
+      try {
+        // 应该先询问一下
+        await this.$dialog.confirm({
+          title: '提示',
+          message: '你确定要退出登录吗' })
+        // 应该将之前的token删除掉
+        this.clearUser()
+        this.$router.push('/login')
+      } catch (error) {
+
+      }
     }
   },
   created () {
