3891035363ae87ec2ceba980974872f19719f13a
kb <kobe@qq.com>
2020-02-17 20:14:52 +0800

    文章详情-评论组件-评论与回复

--- src/views/article/components/comment.vue
+++ src/views/article/components/comment.vue
@@ -28,16 +28,16 @@
       </div>
     </van-list>
     <div class="reply-container van-hairline--top">
-      <van-field v-model="value" placeholder="写评论...">
+      <van-field v-model.trim="value" placeholder="写评论...">
         <van-loading v-if="submiting" slot="button" type="spinner" size="16px"></van-loading>
-        <span class="submit" v-else slot="button">提交</span>
+        <span class="submit" @click="submit()" v-else slot="button">提交</span>
       </van-field>
     </div>
 
     <!-- 回复列表组件 -->
     <!-- 回复 -->
-    <van-action-sheet v-model="showReply" :round="false" class="reply_dialog" title="回复评论">
-      <van-list @load="getReplay" :immediate-check="fasle" v-model="reply.loading" :finished="reply.finished" finished-text="没有更多了">
+    <van-action-sheet @closed = "reply.commentId=null" :round="false" v-model="showReply"  class="reply_dialog" title="回复评论">
+      <van-list @load="getReply" :immediate-check="false" v-model="reply.loading" :finished="reply.finished" finished-text="没有更多了">
         <div class="item van-hairline--bottom van-hairline--top" v-for="reply in reply.list" :key="reply.com_id.toString()">
           <van-image
             round
@@ -64,7 +64,8 @@
 </template>
 
 <script>
-import { getComments } from '@/api/article'
+
+import { getComments, commentOrReply } from '@/api/article'
 export default {
   data () {
     return {
@@ -90,17 +91,67 @@ export default {
     }
   },
   methods: {
+    // 提交评论的方法
+    // 提交评论的方法
+    // 提交评论的方法
+    // 提交评论的方法
+    async submit () {
+      if (!this.value) return false // 表示如果当前评论内容为空就立刻返回
+      this.submiting = true // 将提交状态设置成true 控制用户单位时间内评论的数据次数
+      await this.$sleep() // 强制等待500 毫秒
+      try {
+        // 评论
+        // 一种是对文章进行评论
+        // 一种是对评论进行评论
+        // 如果不为空 继续
+        // 怎么样区分当前是对文章进行评论 还是对评论进行评论
+        // 两种方式 一种方式 通过 showReply的true/false
+        // 一种方式 通过 reply.commentId是存在
+        const data = await commentOrReply({
+        // this.reply.commentId 存在 就要对 评论进行评论  否则传文章ID
+          target: this.reply.commentId ? this.reply.commentId.toString() : this.$route.query.articleId, // 要么是文章id, 要么是 评论id
+          content: this.value, // 评论的内容
+          art_id: this.reply.commentId ? this.$route.query.articleId : null
+        // 如果此时对文章进行评论 art_id不能传 如果是对 评论进行评论 文章ID必须传
+        })
+        // 评论成功怎么处理 刷新页面 => 将评论的数据呈现到 视图上
+        // 需要知道加个哪个数组的最前面
+        if (this.reply.commentId) {
+          // 对评论进行评论
+          this.reply.list.unshift(data.new_obj) // 将数据加到 队首 评论的评论
+          // 如果对评论的进行评论之后, 应该找到该评论 并将 该评论的回复次数 +1
+          // 怎么找到 对应的评论
+          // 如果找到了 comment就是找到的对象 如果找不到 comment就是一个undefined
+          const comment = this.comments.find(item => item.com_id.toString() === this.reply.commentId.toString())
+          comment && comment.reply_count++ // 如果找到了 就对回复数据加1
+        } else {
+          // 对文章进行评论
+          this.comments.unshift(data.new_obj) // 文章评论
+        }
+        this.submiting = false
+        this.value = '' // 清空输入框
+      } catch (error) {
+        console.log(error)
+        this.$gnotify({ type: 'danger', message: '评论失败' })
+      }
+      // 最后去关闭 状态
+      this.submiting = false // 关闭进度条
+    },
+    // 打开回复列表面板
+
     // 打开回复列表面板
     openReply (commentId) {
       this.showReply = true // 弹出面板
       // 进行若干操作
       this.reply.commentId = commentId // 存储当前点击的commentId
-      this.reply.list = []// 清空原有的数据
-      this.reply.offset = null// 重置回复的偏移量 因为每个评论的评论都是从第一页开始
+      this.reply.list = [] // 清空原有的数据
+      this.reply.offset = null // 重置回复的偏移量 因为每个评论的评论都是从第一页开始
       this.reply.finished = false // 设置成原始状态
       this.reply.loading = true // 主动打开加载状态 因为这个时候没有了自动的检查
-      this.getReply()// 开始加载第一页的数据了  开始调用第一页的数据
+      // 开始加载第一页的数据了
+      this.getReply() // 开始调用第一页的数据
     },
+    // 获取评论的评论 也就是获取回复数据的方法
     // 获取评论的评论 也就是获取回复数据的方法
     async getReply () {
       // 加载逻辑
@@ -109,13 +160,12 @@ export default {
         offset: this.reply.offset, // 偏移量
         source: this.reply.commentId.toString() // 代表获取的评论(id)的评论
       })
-      this.reply.list.push(...data.results)
-      // 拿到的data只是请求的第一页数据
+      // 拿到的data只是请求的第一页的数据
       this.reply.loading = false // 评论的评论的加载状态关闭
-      this.reply.finished = data.end_id === data.last_id // 如果last_id等于end_id 表示没有数据可以再加载了
+      this.reply.finished = data.end_id === data.last_id // 如果last_id 等于end_id表示 没有数据可以再加载了
       if (!this.reply.finished) {
-        // 意味着有下一页的数据
-        this.reply.offset = data.last_id // 把当前页德最后一个id给偏移量作为请求下一页的参数依据
+        //  意味着有下一页的数据
+        this.reply.offset = data.last_id // 把当前页的最后一个id给偏移量 作为请求下一页的参数依据
       }
     },
     // 一级评论
